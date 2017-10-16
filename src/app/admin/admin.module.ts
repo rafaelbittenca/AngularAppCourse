1d102ff084d3d17ae183236f3619b88aa5eae7a0
rafaelbittenca <rafaelbittencourt.fns@gmail.com>
2017-10-16 13:04:47 -0400

    modules

---
+++ src/app/admin/admin.module.ts
@@ -0,0 +1,43 @@
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { AuthGuarde } from 'shared/services/auth-guard.service';
+
+import { SharedModule } from './../shared/shared.module';
+import { AdminOrdersComponent } from './components/admin-orders/admin-orders.component';
+import { AdminProductsComponent } from './components/admin-products/admin-products.component';
+import { ProductFormComponent } from './components/product-form/product-form.component';
+import { AdminAuthGuard } from './services/admin-auth-guard.service';
+
+@NgModule({
+  imports: [
+    SharedModule,
+    RouterModule.forChild([
+      { 
+        path: 'admin/products/new', 
+        component: ProductFormComponent, 
+        canActivate: [AuthGuarde, AdminAuthGuard] 
+      },
+      { 
+        path: 'admin/products/:id', 
+        component: ProductFormComponent, 
+        canActivate: [AuthGuarde, AdminAuthGuard] 
+      },
+      { 
+        path: 'admin/products', 
+        component: AdminProductsComponent, 
+        canActivate: [AuthGuarde, AdminAuthGuard] 
+      },
+      { 
+        path: 'admin/orders', 
+        component: AdminOrdersComponent, 
+        canActivate: [AuthGuarde, AdminAuthGuard] 
+      }
+    ])            
+  ],
+  declarations: [
+    ProductFormComponent,
+    AdminProductsComponent,
+    AdminOrdersComponent,
+  ]
+})
+export class AdminModule { }
